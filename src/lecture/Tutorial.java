1468cf049b4d1e3c2228ae0d58662433a727645e
dipersonmaharjan-alt <diperson.maharjan@bicnepal.edu.np>
2025-12-25 12:54:14 +0545

    gfdfg

---
+++ src/lecture/Tutorial.java
@@ -0,0 +1,16 @@
+package lecture;
+import tutorial2.AreaCalculator; 
+
+
+
+public class Tutorial extends AreaCalculator{
+
+	void display() {
+		System.out.println(geta());
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+
+
+}
