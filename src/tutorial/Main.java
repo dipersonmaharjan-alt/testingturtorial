e87ef8da82b4c193859f6a387f6741d7a985f126
dipersonmaharjan-alt <diperson.maharjan@bicnepal.edu.np>
2025-12-27 19:14:20 +0545

    Diperson

---
+++ src/tutorial/Main.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class Main {
+	/**
+	 * 
+	 * @param num1 takes first number 
+	 * @param num2 takes second number
+	 * @return sum of num1 and num2
+	 * 
+	 * */
+	public int addNumber(int num1, int num2) {
+		return num1 + num2;
+	}
+}
